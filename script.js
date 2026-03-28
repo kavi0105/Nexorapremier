1f2a6a5929eefa4c93c47a0ffe1c45f745f29226
kavi0105 <kavishkalochna@gmail.com>
2026-03-28 23:34:15 +0530

    updated website pages and styles

--- script.js
+++ script.js
@@ -25,6 +25,29 @@ function revealOnScroll() {
     }
   });
 }
+const themeToggle = document.getElementById("themeToggle");
+
+window.addEventListener("load", () => {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "light") {
+    document.body.classList.add("light-mode");
+  } else {
+    document.body.classList.remove("light-mode");
+  }
+});
+
+if (themeToggle) {
+  themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+
+    if (document.body.classList.contains("light-mode")) {
+      localStorage.setItem("theme", "light");
+    } else {
+      localStorage.setItem("theme", "dark");
+    }
+  });
+}
 
 window.addEventListener("scroll", revealOnScroll);
 window.addEventListener("load", revealOnScroll);
